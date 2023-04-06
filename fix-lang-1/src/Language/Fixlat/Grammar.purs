1345863a04840b60b40342211a07809e845cdada
Henry Blanchette <henry@monkeyface.com>
2023-04-07 01:51:44 +0800

    rename some local funs

--- fix-lang-1/src/Language/Fixlat/Grammar.purs
+++ fix-lang-1/src/Language/Fixlat/Grammar.purs
@@ -270,27 +270,27 @@ instance partialOrdTermSort :: PartialOrd (Term Sort) where
       term2' = substTerm sigma term2
 
       sr = open term1'
-    go sr term1' term2'
+    go1 sr term1' term2'
     where
-    go _sr (VarTerm v1) (VarTerm v2)
+    go1 _sr (VarTerm v1) (VarTerm v2)
       | v1.name == v2.name = pure EQ
 
-    go UnitSort (UnitTerm u1) (UnitTerm u2) = pure $ compare u1.unit u2.unit
+    go1 UnitSort (UnitTerm u1) (UnitTerm u2) = pure $ compare u1.unit u2.unit
 
-    go BoolSort (BoolTerm b1) (BoolTerm b2) = pure $ compare b1.bool b2.bool
+    go1 BoolSort (BoolTerm b1) (BoolTerm b2) = pure $ compare b1.bool b2.bool
 
-    go (TupleSort tup) (TupleTerm tup1) (TupleTerm tup2) = case tup.mb_ord of
+    go1 (TupleSort tup) (TupleTerm tup1) (TupleTerm tup2) = case tup.mb_ord of
       Just LexicographicTupleOrdering -> do
         Array.foldM
           ( case _ of
-              EQ -> \(sr /\ (tm1 /\ tm2)) -> go sr tm1 tm2
+              EQ -> \(sr /\ (tm1 /\ tm2)) -> go1 sr tm1 tm2
               o -> const (pure o)
           )
           EQ
           (tup.components `Array.zip` (tup1.components `Array.zip` tup2.components))
       Nothing -> unsafeCrashWith $ "comparePartial: uninstantiated tuple ordering sort:" <> "\n  sr = " <> show (TupleSort tup) <> "\n  tup1 = " <> show (TupleTerm tup1) <> "\n  tup2 = " <> show (TupleTerm tup2)
 
-    go _sr _tm1 _tm2' = empty
+    go1 _sr _tm1 _tm2' = empty
 
     substTerm :: forall a. Map.Map Name (Term a) -> Term a -> Term a
     substTerm sigma (VarTerm v) = fromMaybe (VarTerm v) (Map.lookup v.name sigma)
@@ -303,25 +303,25 @@ instance partialOrdTermSort :: PartialOrd (Term Sort) where
     unifySorts sr1 sr2 = sr1 == sr2
 
     unifyTerms :: Term Sort -> Term Sort -> Maybe (Map.Map Name (Term Sort))
-    unifyTerms term1 term2 = execStateT (go term1 term2) Map.empty
+    unifyTerms term1 term2 = execStateT (go2 term1 term2) Map.empty
       where
-      go tm1 tm2
-        | unifySorts (open tm1) (open tm2) = go' tm1 tm2
+      go2 tm1 tm2
+        | unifySorts (open tm1) (open tm2) = go3 tm1 tm2
         | otherwise = throwBug "unifyTerms" $ "attempted to unify terms with different sorts:" <> "\n  tm1 = " <> show tm1 <> "\n  tm2 = " <> show tm2
 
-      go' (VarTerm v1) tm2 = subst v1.name tm2
+      go3 (VarTerm v1) tm2 = subst v1.name tm2
 
-      go' tm1 (VarTerm v2) = subst v2.name tm1
+      go3 tm1 (VarTerm v2) = subst v2.name tm1
 
-      go' (UnitTerm u1) (UnitTerm u2)
+      go3 (UnitTerm u1) (UnitTerm u2)
         | u1.unit == u2.unit = pure unit
 
-      go' (BoolTerm b1) (BoolTerm b2)
+      go3 (BoolTerm b1) (BoolTerm b2)
         | b1.bool == b2.bool = pure unit
 
-      go' (TupleTerm tup1) (TupleTerm tup2) = for_ (tup1.components `Array.zip` tup2.components) (uncurry go)
+      go3 (TupleTerm tup1) (TupleTerm tup2) = for_ (tup1.components `Array.zip` tup2.components) (uncurry go2)
 
-      go' _ _ = mempty
+      go3 _ _ = mempty
 
       subst x tm = modify_ $ Map.insert x tm
 
